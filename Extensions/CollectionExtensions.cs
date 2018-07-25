3da23e756d314a0c626f05a4402661800aa77366
Ivan Yochev <vani172@gmail.com>
2018-07-25 22:40:45 +0300

    collection extensions

---
+++ Extensions/CollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyMVVM.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
+        {
+            return new ObservableCollection<T>(source);
+        }
+
+        public static void ForEach<T>(this ICollection<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}

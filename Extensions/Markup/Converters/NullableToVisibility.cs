28a6abb2c8653498c8ef27d6d3606fdaabcf1214
Ivan Yochev <vani172@gmail.com>
2018-08-31 14:52:36 +0300

    converters

---
+++ Extensions/Markup/Converters/NullableToVisibility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TinyMVVM.Extensions.Markup.Converters
+{
+    public class NullableToVisibility : MarkupExtension, IValueConverter
+    {
+        public bool Invert { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = value == null && !Invert;
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
